690e7ce1e415977b77b5305abbecc29313adcd7c
BimmerHex <bimmerhex@gmail.com>
2024-09-28 16:48:58 +0300

    Coin için SFX ve Animasyon eklendi.

--- assets/items/scripts/coin.gd
+++ assets/items/scripts/coin.gd
@@ -2,8 +2,9 @@ extends Area2D
 
 
 @onready var game_manager: Node = %GameManager
+@onready var coin_animation: AnimationPlayer = $CoinAnimation
 
 
 func _on_body_entered(_body: Node2D) -> void:
 	game_manager.add_coin()
-	queue_free()
+	coin_animation.play("pickup_coin")
